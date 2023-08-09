17911d5d30f7b06eebaf7fe9ceb430685969e9d7
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-08-10 00:05:03 +0200

    connectGet.php & ConnectInsert.php

---
+++ src/connectGet.php
@@ -0,0 +1,37 @@
+<html>
+<body>
+
+<?php
+
+$dbname = 'id20158736_weatheresp';
+$dbuser = 'id20158736_admin';  
+$dbpass = 'WnFnE6Pa9LZ6LF!'; 
+$dbhost = 'localhost'; 
+
+
+$connect = @mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+if(!$connect){
+	echo "Error: " . mysqli_connect_error();
+	exit();
+}
+echo "Connection Success!<br><br>";
+
+$table = $_POST['table'];
+$maincolumn = $_POST['maincolumn'];
+$mainvalue = $_POST['mainvalue'];
+$getcolumn = $_POST['getcolumn'];
+
+/*
+$table = "newsapplication";
+$column = "test";
+$value = "a";
+*/
+
+echo mysqli_query($connect,"SELECT $getcolumn FROM $table WHERE $maincolumn = $mainvalue");
+
+echo "Insertion Success!<br>";
+
+?>
+</body>
+</html>
