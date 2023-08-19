9f7d50c9a4c0cc3b1360d6c159a2bc460908aa5c
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-08-20 00:02:22 +0200

    CheckEmail.php

---
+++ src/MySQLBridge/checkEmail.php
@@ -0,0 +1,95 @@
+<html>
+<body>
+
+<?php
+
+
+
+
+/**
+ * 
+ * Create Database connection
+ * 
+ */
+
+$dbname = 'id20158736_weatheresp';
+$dbuser = 'id20158736_admin';  
+$dbpass = 'WnFnE6Pa9LZ6LF!'; 
+$dbhost = 'localhost'; 
+
+
+$connect = @mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+if(!$connect){
+	echo "Error: " . mysqli_connect_error();
+	exit();
+}
+echo "Connection Success!<br><br>";
+
+
+
+
+
+/**
+ * 
+ * JSON Post request data 
+ * 
+ */
+
+ $table = $_GET['table'];
+ $email = $_GET['email'];
+ $authCodeByUser = $_GET['authcodetocheck']; //authcode that must be delivered in the post statement
+
+
+
+
+
+ /**
+  * 
+  * Verify Authcode
+  * 
+  */
+
+ $sql = "SELECT authcode FROM auth WHERE available = true";
+ $result = $connect->query($sql);
+
+ if ($result->num_rows > 0) {
+     while($row = $result->fetch_assoc()) {
+         $authcode = $row["authcode"];
+         if($authCodeByUser == $authcode){ // authcode of user equals the "real" authcode
+            $booleanvalue = true;
+         }
+     }
+
+ } else {
+     echo "No Authcode found";
+     exit(11);
+ }
+ if($booleanvalue != true){
+    exit(10);
+ }
+
+
+
+
+
+ /**
+  * 
+  * Check if username exists
+  * 
+  */
+
+ $sql = "SELECT username FROM $table WHERE email = \"$email\"";
+ $result = $connect->query($sql);
+
+ if ($result->num_rows > 0) {
+     echo "Email is used";
+     exit(12);
+ } else {
+     echo "Email is free";
+     exit(13);
+ }
+
+?>
+</body>
+</html>
