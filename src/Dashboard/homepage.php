d33e5c0b0a212bd331d436bae3bb0557812f0600
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-08-25 00:05:17 +0200

    Homepage user list

--- src/Dashboard/homepage.php
+++ src/Dashboard/homepage.php
@@ -5,23 +5,49 @@
 <html>
 <head>
     <title>Homepage</title>
+    <link rel="stylesheet" type="text/css" href="CSS/Homepage.css">
 </head>
 <body>
     <?php
     if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
         echo "<h1>News-Application Dashboard!</h1>";
 
-        $buttonText = "Log out";
-        echo "<form method='post' action=''>";
-        echo "<input type='submit' name='submit' value='$buttonText'>";
-        echo "</form>";
+        // Start the table
+        echo "<table border='1'>
+                <tr>
+                    <th>UUID</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Username</th>
+                    <th>Birthday</th>
+                    <th>Signup</th>
+                    <th>Role</th>
+                </tr>";
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
-            echo "Der Knopf wurde angeklickt!";
-            session_start();
-            session_unset();
-            header("Location: ".$_SERVER['PHP_SELF']);
+        $users = array();
+
+        $sql = "SELECT * FROM newsuser";
+        $result = $connect->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $users[] = [$row["uuid"],$row["email"],$row["password"],
+                            $row["username"],$row["birthday"],$row["signup"],$row["role"]];
+            }
+
+        } else {
+            exit(11);
         }
+
+        foreach ($users as $usera) {
+            echo "<tr>";
+            foreach ($usera as $value) {
+                echo "<td>$value</td>";
+            }
+            echo "</tr>";
+        }
+
+        echo "</table>";
 
     } else {
         echo "<h1>Zugriff verweigert</h1>";
